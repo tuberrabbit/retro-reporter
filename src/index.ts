3493d3dbf011bd0a9b398fb4e841a0ede1bcb585
tuber <btu@nocode.com>
2019-09-06 16:59:01 +0800

    [tuber] use start keep reduce stop dimension

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,20 @@
 import * as fs from 'fs';
 
-import {
-  getTitle,
-  summary,
-  well,
-  lessWell,
-  confused,
-  actions,
-  notes,
-  parser,
-  getLists,
-} from './templates';
+import { actions, getLists, getTitle, keep, notes, parser, reduce, start, stop, summary, } from './templates';
 
-const [wellList, lessWellList, confusedList, actionsList] = getLists();
+const [startList, keepList, reduceList, stopList, actionsList] = getLists();
 
 const md = parser([
   { h1: getTitle() },
   { p: summary },
-  { h2: well },
-  { ul: wellList },
-  { h2: lessWell },
-  { ul: lessWellList },
-  { h2: confused },
-  { ul: confusedList },
+  { h2: start },
+  { ul: startList },
+  { h2: keep },
+  { ul: keepList },
+  { h2: reduce },
+  { ul: reduceList },
+  { h2: stop },
+  { ul: stopList },
   { h2: actions },
   { ul: actionsList },
   { notes: notes },
